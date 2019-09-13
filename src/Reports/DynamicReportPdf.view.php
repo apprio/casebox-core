28d4f78299a8c772e416410ff8abeb945b8daa71
Daniel Stoudt <dstoudt@apprioinc.com>
2019-09-13 16:06:18 +0000

    Merged PR 111: Replaced placeholder with real code
    For export - replaced placeholder with real code.
    
    Related work items: #547

--- src/Reports/DynamicReportPdf.view.php
+++ src/Reports/DynamicReportPdf.view.php
@@ -8,14 +8,39 @@
     <body>
         <div class="container box-container">
         	<?php 
+        	use \koolreport\widgets\koolphp\Table; 
+			use \koolreport\widgets\google\BarChart;
+			use \koolreport\widgets\google\ColumnChart;     
 ?>
 
 <div class="text-center">
-    <h1>Dynamic Report</h1>
-    <h4>This report shows daily report totals by location</h4>
+    <h1><?php echo($this->params['title']);?></h1>
+    <h4><?php echo($this->params['secondarytitle']);?></h4>
 </div>
 <hr/>
-        
+			<?php
+			if (isset($this->params['barchart']))
+			{
+				$this->params['barchart']['dataStore'] = $this->dataStore('groupDs');
+				//print_r($this->params['table']);
+				BarChart::create($this->params['barchart'],false);
+
+			}
+			?>
+        			<?php
+			if (isset($this->params['table']))
+			{
+				if (isset($this->params['table']['title']))
+				{
+					echo("<h2>".(isset($this->params['table']['title'])?$this->params['table']['title']:'')."</h2>");	
+				}
+				echo("<div class=\"table-responsive\">");
+				$this->params['table']['dataStore'] = $this->dataStore('reportDs');
+				//print_r($this->params['table']);
+				Table::create($this->params['table'],false);
+				echo("</h2></div>");
+			}
+			?>
         		<br><br><br>
 
     </body>
