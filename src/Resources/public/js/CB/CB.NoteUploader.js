1a69604853d333bd97e4228f59e7a34614262d56
Irene Camunag <icamunag>
2021-02-02 10:29:46 -0500

    Supervisor/Supervisee Assigning

--- src/Resources/public/js/CB/CB.NoteUploader.js
+++ src/Resources/public/js/CB/CB.NoteUploader.js
@@ -51,7 +51,6 @@ Ext.define('CB.NoteUploader', {
         // this.autoCloseTask.delay(1000*60*5);
 		this.cards.getLayout().setActiveItem(0);
         CB_User.getProfileData(false, this.onGetData, this);
-
     }
     ,onGetData: function(r, e){
         if(!r) {
@@ -112,6 +111,7 @@ Ext.define('CB.NoteForm', {
                 scope: this
                 ,afterrender: this.onAfterRender
                 ,change: this.onChange
+                ,verify: this.onVerifyEvent
             }
         });
 
@@ -203,7 +203,51 @@ Ext.define('CB.NoteForm', {
         Ext.apply(this.data, this.getForm().getValues());
 
         this.grid.readValues();
-        CB_User.saveProfileData(this.data, this.onSaveProcess, this);
+
+        if (Ext.isDefined(this.data.data.user_role.childs)) {
+          if (this.data.data.user_role.childs.assignedworker.indexOf(',') != -1) {
+             var workers = this.data.data.user_role.childs.assignedworker.split(',');
+             for (var i=0; i < workers.length; i++) {
+               var workerId = {id: workers[i]};
+               CB_UsersGroups.getUserData({data: workerId, assign: true}, this.onGetData, this);
+             }
+         }
+          else {
+            var workerId = {id: this.data.data.user_role.childs.assignedworker};
+            CB_UsersGroups.getUserData({data: workerId, assign: true}, this.onGetData, this);
+          }
+        }
+
+    }
+
+    ,onGetData: function(r, e){
+        if(!r) {
+            return;
+        }
+        if (r.success !== true) {
+            if(r.verify) {
+                this.fireEvent('verify', this);
+            }
+            return false;
+        }
+        if (Ext.isDefined(r.data.data.assignedsupervisor) && r.data.data.assignedsupervisor != this.data.id) {
+          var data = this.data;
+          var info = this;
+          var workerId = r.data.id;
+          Ext.Msg.confirm('Assign Worker', r.data.title + ' is already has an assigned supervisor. <br><center>Assign to yourself?', function(btn){
+            if (btn == 'yes'){
+              data.data.user_role.childs.assignedworker = workerId;
+              Ext.apply(data, info.getForm().getValues());
+              info.grid.readValues();
+              CB_User.saveProfileData(data, info.onSaveProcess, info);
+            } else {
+                alert('Please change your selection.');
+            }
+          });
+        }
+        else {
+          CB_User.saveProfileData(this.data, this.onSaveProcess, this);
+        }
     }
 
     ,onSaveProcess: function(r, e){
@@ -241,6 +285,11 @@ Ext.define('CB.NoteForm', {
         var bbar = this.dockedItems.getAt(0);
         bbar.items.getAt(0).setDisabled(!this._isDirty);
         bbar.items.getAt(1).setDisabled(!this._isDirty);
+    }
+
+    ,onVerifyEvent: function(cmp) {
+        this.destroy();
+        Ext.Msg.alert(L.Info, 'User management session has expired. Please access it and authenticate again.');
     }
 });
 
